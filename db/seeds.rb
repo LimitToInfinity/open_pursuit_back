f9a16f925ac747e5015d5cbfed4b9825b028e6b2
github username <github email address>
2019-08-19 12:45:42 -0600

    First model for heroku test.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hero.create({name: "Dawg", power: "Freestyle"})
+Hero.create({name: "Citizen", power: "Vote"})
